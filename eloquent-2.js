b4a8465bca5192bcbf25335f693c9b8800de02e3
zaine kingi <zaine.kingi@thewarehouse.co.nz>
2018-04-21 10:33:10 +1200

    add solution for exercise 1 - looping triangle.

---
+++ eloquent-2.js
@@ -0,0 +1,16 @@
+/* ---------------------------------------
+    EXERCISE 1: LOOPING A TRIANGLE
+--------------------------------------- */
+
+// Your code here.
+// initiate bindings.
+let str = "",
+    i = 0;
+while(i < 7) {
+  // add an extra hash to the string.
+  str = str + "#";
+  // output the string to the console.
+  console.log(str);
+  // increment the count.
+  i ++;
+}
