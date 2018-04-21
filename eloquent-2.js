99fc1f3c5f90869899cfedae1a94c5085631e0c8
zaine kingi <zaine.kingi@thewarehouse.co.nz>
2018-04-21 22:13:47 +1200

    add exercise 3 - chess board.

--- eloquent-2.js
+++ eloquent-2.js
@@ -50,3 +50,55 @@ while(i <= 100) {
   	console.log(i);
   	i++;
 }
+
+/* ----------------------------------------
+    EXERCISE 3: CHESS BOARD
+---------------------------------------- */
+
+// Your code here.
+
+// make grid function.
+function makeGrid(n) {
+  
+    // initiate bindings.
+    let i = 0,
+        pat = "",
+        patArr = [],
+        gridArr = [];
+    
+    // make the pattern.
+    while(i < n / 2) {
+      // create the pattern.
+      pat = pat + " " + "#";
+          
+      // push to pattern array.
+      patArr.push(pat);
+          
+      // increment the counter.
+      i ++;
+    }
+    
+    // push last item of pattern array to grid array.
+    let e = 0,
+        gridRow = patArr[patArr.length - 1],
+        gridRowRev = gridRow.split("").reverse().join("");
+    
+    // make the grid rows.
+    while(e < n) {
+      // reverse the string for every second row.
+      if(e % 2 == 0) {
+          gridArr.push(gridRowRev);
+            // output grid.
+            console.log(gridArr[e]);
+            e ++;
+      } else {
+          gridArr.push(gridRow);
+            // output grid.
+            console.log(gridArr[e]);
+            e ++;
+      } 
+    }
+  }
+  
+  // call make grid method.
+  makeGrid(8);
