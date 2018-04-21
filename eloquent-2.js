d7898505fb35e5e1310da5a2aefb3d491ea0743c
zaine kingi <zaine.kingi@thewarehouse.co.nz>
2018-04-21 12:23:20 +1200

    add exercise 2 - fizzbuzz.

--- eloquent-2.js
+++ eloquent-2.js
@@ -14,3 +14,39 @@ while(i < 7) {
   // increment the count.
   i ++;
 }
+
+/* --------------------------------------
+    EXERCISE 2: FIZZBUZZ
+-------------------------------------- */
+
+// Your code here.
+// initiate bindings.
+let i = 1;
+
+// loop.
+while(i <= 100) {
+	// check if the count is divisable by 3 & 5.
+  	if(i % 3 === 0 && i % 5 === 0) {
+		// output message.
+      	console.log("FizzBuzz");
+		// increase count.
+      	i ++;
+      
+    // check if the count is divisable by 3.
+    } else if(i % 3 === 0) {
+		// output message.
+      	console.log("Fizz");
+		// increase count.
+      	i ++;
+      
+    // check if the count is divisable by 5.
+    } else if(i % 5 === 0){
+		// output message.
+      	console.log("Buzz");
+		// increase count.
+      	i ++;
+    }
+	// output default message.
+  	console.log(i);
+  	i++;
+}
